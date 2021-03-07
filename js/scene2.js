ebd82511ccf4af6f53138fffc1989dd112b50e27
jt1222 <jaytinosaprogramming@gmail.com>
2021-03-07 12:27:01 +0800

    Modified scene2

--- js/scene2.js
+++ js/scene2.js
@@ -1,41 +1,15 @@
 monogatari.script ({
 	'scene2': [
-        "y Hello here are the lessons that I prepare for you.",
-        "y You can choose any lesson you wanted to start",
+        "y Choose a lesson you wanted to start with.",
         {'Choice': {
-            'Lesson1': {
-                'Text': 'Basic Concepts',
+            'Beginner': {
+                'Text': 'Beginner',
                 'Do': 'jump lesson1',
                 'class': "button-style"
             },
-            'Lesson2': {
-                'Text': 'Conditionals and Loops',
+            'Advance': {
+                'Text': 'Advance',
                 'Do': 'jump lesson2',
-                'class': "button-style"
-            },
-            'Lesson3': {
-                'Text': 'Arrays',
-                'Do': 'jump lesson3',
-                'class': "button-style"
-            },
-            'Lesson4': {
-                'Text': 'Classes and Objects',
-                'Do': 'jump lesson4',
-                'class': "button-style"
-            },
-            'Lesson5': {
-                'Text': 'Object-Oriented Programming Concepts',
-                'Do': 'jump lesson5',
-                'class': "button-style"
-            },
-            'Lesson6': {
-                'Text': 'Exceptions,Lists and Threads',
-                'Do': 'jump lesson6',
-                'class': "button-style"
-            },
-            'Lesson7': {
-                'Text': 'Files',
-                'Do': 'jump lesson7',
                 'class': "button-style"
             }
         }}
