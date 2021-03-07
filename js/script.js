6d7035a7018c7e8cae74e7edea57b6e6768eee62
jt1222 <jaytinosaprogramming@gmail.com>
2021-03-07 12:01:21 +0800

    Modified monogatari.css added scripts in index.html added js files

--- js/script.js
+++ js/script.js
@@ -78,6 +78,9 @@ monogatari.characters ({
 	'y': {
 		name: 'Yui',
 		color: '#5bcaff'
+	},
+	'player': {
+		name: '{{player.name}}'
 	}
 });
 
@@ -85,7 +88,6 @@ monogatari.script ({
 	// The game starts here.
 	'Start': [
 		'show scene #f7f6f6 with fadeIn',
-		'show notification Welcome',
 		{
 			'Input': {
 				'Text': 'What is your name?',
@@ -110,37 +112,6 @@ monogatari.script ({
 				'Warning': 'You must enter a name!'
 			}
 		},
-		'y Hi {{player.name}} Welcome to Monogatari!',
-		{
-			'Choice': {
-				'Dialog': 'y Have you already read some documentation?',
-				'Yes': {
-					'Text': 'Yes',
-					'Do': 'jump Yes'
-				},
-				'No': {
-					'Text': 'No',
-					'Do': 'jump No'
-				}
-			}
-		}
-	],
-
-	'Yes': [
-		'y Thats awesome!',
-		'y Then you are ready to go ahead and create an amazing Game!',
-		'y I can’t wait to see what story you’ll tell!',
-		'end'
-	],
-
-	'No': [
-
-		'y You can do it now.',
-
-		'show message Help',
-
-		'y Go ahead and create an amazing Game!',
-		'y I can’t wait to see what story you’ll tell!',
-		'end'
+		'jump scene1'
 	]
 });
